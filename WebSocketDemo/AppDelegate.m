86b54135ab42c7c31715cc5011f38bf065eef0c7
yz--lmy <996913797@qq.com>
2015-08-18 10:50:55 +0800

    创建WebSocketDemo

--- WebSocketDemo/AppDelegate.m
+++ WebSocketDemo/AppDelegate.m
@@ -14,6 +14,8 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    RootViewController * rootVC = [[RootViewController alloc] init];
+    self.window.rootViewController = rootVC;
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ WebSocketDemo/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  WebSocketDemo
+//
+//  Created by sjkx123456 on 15/8/17.
+//  Copyright (c) 2015年 sjkx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
